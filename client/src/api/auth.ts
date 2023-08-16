ca3ea86a88aebd0f07d25e4d98272f413eb3453b
andy <takuyarossi77@gmail.com>
2023-08-16 21:16:57 +0900

    add node version

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/auth';
+const API_URL = import.meta.env.VITE_APP_BASE_URL;
 
 export const login = async (email: string, password: string) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
+  const response = await axios.post(`${API_URL}/auth/login`, { email, password });
   return response.data;
 };
 
--- client/src/api/income.ts
+++ client/src/api/income.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/acounting";
+const API_URL = import.meta.env.VITE_APP_BASE_URL;
 const token = localStorage.getItem("token");
 
 export const getIncome = async (userId: number) => {
-    const response = await axios.get(`${API_URL}/income/${userId}`, { headers: { 'Authorization': `Bearer ${token}` } });
+    const response = await axios.get(`${API_URL}/acounting/income/${userId}`, { headers: { 'Authorization': `Bearer ${token}` } });
     return response.data.income;
 }
 
 export const addIncomes = async (userId: number | undefined, name: string | undefined, amount: number | undefined) => {
-    const response = await axios.post(`${API_URL}/income`, {
+    const response = await axios.post(`${API_URL}/acounting/income`, {
         user_id: userId,
         name: name,
         amount: amount
@@ -18,12 +18,12 @@ export const addIncomes = async (userId: number | undefined, name: string | unde
 }
 
 export const deleteIncomes = async (id: number) => {
-    const response = await axios.delete(`${API_URL}/delete/income/${id}`, { headers: { 'Authorization': `Bearer ${token}` } });
+    const response = await axios.delete(`${API_URL}/acounting/delete/income/${id}`, { headers: { 'Authorization': `Bearer ${token}` } });
     return response.data;
 }
 
 export const changeIncomeData = async (id: number, name: string, amount: number) => {
-    const response = await axios.put(`${API_URL}/change/income/${id}`, {
+    const response = await axios.put(`${API_URL}/acounting/change/income/${id}`, {
         name: name,
         amount: amount
     }, { headers: { 'Authorization': `Bearer ${token}` } });

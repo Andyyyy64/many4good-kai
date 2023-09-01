315091f3c7c5fb0ea6ec6dbaa7d8c0a06a0da47a
Andyyyy64 <takuyarossi77@gmail.com>
2023-09-01 19:03:46 +0900

    fuck git

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://many4good-kai-server.onrender.com"
+const API_URL = import.meta.env.VITE_APP_BASE_URL;
 
 export const login = async (email: string, password: string) => {
   const response = await axios.post(`${API_URL}/auth/login`, { email, password });
